68bcaa61d6461d5ebe4b842ec68d33d381036c0e
ktr0328 <zf7heatpa@gmail.com>
2018-08-16 02:09:21 +0900

    feat: service server setup => step 1 end #3
    - htmlを返すサーバー構築
      - 簡素なページ全体の作成
    - 認証サーバーからprivate keyの取得終了

--- src/ca/index.js
+++ src/ca/index.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 // encrypt
 app.post('/', (req, res, next) => {
   const number = req.body.number
--- src/service/index.js
+++ src/service/index.js
@@ -1,1 +1,12 @@
-//
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.argv[2] || 4000
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+})
